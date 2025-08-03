7dd75a5fd93576bed88cfceddbe88dd5050d9168
Vlastislav Urikh <vlastislav.u@gmx.de>
2025-08-03 21:21:57 +0200

    Add controls to view

--- PageScraper/MainViewModel.cs
+++ PageScraper/MainViewModel.cs
@@ -1,8 +1,62 @@
-﻿using PageScraper.Basics;
+﻿using CommunityToolkit.Mvvm.Input;
+using PageScraper.Basics;
 
 namespace PageScraper;
 
 public class MainViewModel : ViewModelBase
 {
+    private string websiteUrl;
+    private bool scrapeImages;
+    private bool scrapeLinks;
 
+    public RelayCommand StartScrapingCommand { get; }
+
+    public MainViewModel()
+    {
+        StartScrapingCommand = new RelayCommand(StartScraping);
+    }
+
+    private async void StartScraping()
+    {
+        // ToDo: Implement logic for scraping
+    }
+
+    public string WebsiteUrl
+    {
+        get => websiteUrl;
+        set
+        {
+            if (websiteUrl != value)
+            {
+                websiteUrl = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public bool ScrapeImages
+    {
+        get => scrapeImages;
+        set
+        {
+            if (scrapeImages != value)
+            {
+                scrapeImages = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+    
+    public bool ScrapeLinks
+    {
+        get => scrapeLinks;
+        set
+        {
+            if (scrapeLinks != value)
+            {
+                scrapeLinks = value;
+                OnPropertyChanged();
+            }
+        }
+    }
 }
